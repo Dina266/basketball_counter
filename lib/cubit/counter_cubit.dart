f40260c3520582b3f52cf400935b0e7bc4b8b449
Dina Ahmed <denaahmed340@gmail.com>
2023-12-06 21:40:14 +0200

    coding cubit logic

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,7 +1,22 @@
 import 'package:basketball_points_counter/cubit/counter_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CounterCubit extends Cubit<CounterState>{
-  CounterCubit() : super (CounterAIncrementState());
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterAIncrementState());
 
+  int teamAPoints = 0;
+  int teamBPoints = 0;
+
+  void teamIncrement({required String team, required int buttomNum}) {
+    if (team == 'A') {
+      teamAPoints += buttomNum;
+      emit(CounterAIncrementState());
+
+    } else {
+
+      teamBPoints += buttomNum;
+      emit(CounterBIncrementState());
+
+    }
+  }
 }
